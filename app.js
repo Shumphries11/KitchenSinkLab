d697f589f74f1b15d2ef0d6e680696adc2783978
shumphries11 <santitahumphries@gmail.com>
2022-01-15 12:40:42 -0600

    UPTDATE

---
+++ app.js
@@ -0,0 +1,80 @@
+let firstName = "Santita";
+
+const numberOfStates = 50;
+
+let num1 = 5 + 4;
+
+// This is a JavaScript file
+
+function sayHello() {
+    alert("Hello World!");
+}
+
+sayHello();
+
+
+function checkAge(name, age) {
+    if(age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+}
+
+checkAge("Charles", 21);
+
+checkAge("Abby", 27);
+
+checkAge("James", 18);
+
+checkAge("John", 17);
+
+let favoriteVegetables = ["Avocado", "Broccoli", "Cabbage", "Kale"];
+
+for( let a = 0; a < favoriteVegetables.length; a++) {
+    console.log(favoriteVegetables[a]);
+}
+
+let pet = {
+    name:"Koda",
+    breed: "Toy Poddle"
+};
+
+console.log(pet.name);
+console.log(pet.breed);
+
+let family = [
+    {
+        name: "Tristan",
+        age: 6
+    },
+    {
+        name: "Sandra",
+        age: 68
+    },
+    {
+        name: "Chloe",
+        age: 11
+    },
+    {
+        name: "Juanita",
+        age: 21
+    },
+    {
+        name: "Cyriah",
+        age: 6
+    }
+];
+
+family.forEach(checkAge(family.name, family.age));
+
+
+function getLength(word) {
+    console.log(word.length);
+    if(word.length % 2 == 0) {
+        console.log("The worls is nice and even!");
+    } else {
+        console.log("The World is an odd place");
+    }
+}
+
+getLength("Hello World");
+
